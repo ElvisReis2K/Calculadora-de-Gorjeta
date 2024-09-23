96759da26ed2991c073e3db9db98ef5ad66c2e1b
Elvis Reis <elvis_vante@hotmail.com>
2024-09-23 15:32:33 -0300

    feat: Introdução da função de formatação e divsão

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ function formatMoney(value) {
     value = Math.ceil(value * 100) / 100
     value = value.toFixed(2)
     return '$ ' + value 
-
-
-    
 }
+
+function formatSplit(value) {
+    if (value == 1) return value + ' person'
+    return value + ' people'
+    }
